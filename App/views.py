b207717b8866d39568b3f9e0e62c5a21cad81853
ashwiniiyadavv <ashwiniiyadavv@gmail.com>
2024-11-24 20:09:49 +0530

    Urls and views handled

--- App/views.py
+++ App/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index (request):
+    if request.method=="GET":
+        return render(request,'index.html')
+    elif request.method=="POST":
+        return render(request,'response.html')
+
