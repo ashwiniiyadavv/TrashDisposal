daca6e05d8adbb3350cc13dc1cddcf801fe65fc0
chaitraDev <chaitravghule@gmail.com>
2024-11-24 20:32:17 +0530

    separated the logic to find waste or not and suggestions

--- App/views.py
+++ App/views.py
@@ -4,12 +4,21 @@ api_key = input("Enter your API key: ")
 genai.configure(api_key=api_key)
 model = genai.GenerativeModel('gemini-1.5-flash',system_instruction="You are an environmentalist and recycling expert")
 
-def generate_response(obj_category:str, obj_type:str) -> str:
+def check_recyclable(obj_category:str, obj_type:str) -> str:
     prompt = f""" 
                     We have {obj_category} which is {obj_type},
-                    if it can be re-used at home, suggest ideas to recyle as 1. 2. 3.
-                    also give ways to dispose it off as 1. 2. 3.,
-                    
+                    Answer as recylable: Yes or no, 
+              """
+    response = model.generate_content(prompt)
+    print(response.text)
+    return "Yes" in response.text 
+
+def suggest(is_recyclable,object):
+    # TODO: add country guidelines in the prompt
+    action = "recyle and re-use at home" if is_recyclable else "dispose"
+    print(action)
+    prompt = f"""
+                  We have {object}, suggest ways to {action} it as 1. 2. 3.
               """
     response = model.generate_content(prompt)
     print(response.text)
@@ -17,5 +26,7 @@ def generate_response(obj_category:str, obj_type:str) -> str:
 
 category = input("Enter category of object (eg. bottle): ")
 type = input("Enter type of waste (dry, wet, hazardeous): ")
+# TODO: also fetch country guidelines for this specific object and then send to the function
 
-generate_response(category,type)
+is_recylable = check_recyclable(category,type)
+suggest(is_recylable,category)
